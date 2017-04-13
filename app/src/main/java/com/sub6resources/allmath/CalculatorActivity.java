b77a5d2f2ddfbc85c78fbc73bbd9b781d104141d
Matthew Whitaker <matthew.w3000@gmail.com>
2017-04-13 14:19:14 -0600

    More stuff works! Yaay!

--- app/src/main/java/com/sub6resources/allmath/CalculatorActivity.java
+++ app/src/main/java/com/sub6resources/allmath/CalculatorActivity.java
@@ -2,15 +2,28 @@ package com.sub6resources.allmath;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
+import com.sub6resources.allmath.math.EquationParser;
+
 public class CalculatorActivity extends AppCompatActivity {
 
+    public String PLUS = "+";
+    public String MINUS = "-";
+    public String TIMES = "×";
+    public String DIVIDE = "÷";
+
+
+    TextView output;
+    TextView answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        output = (TextView)findViewById(R.id.outputText);
+        answer = (TextView)findViewById(R.id.errorText);
     }
     String equationString = "0";
 
@@ -45,9 +58,30 @@ public class CalculatorActivity extends AppCompatActivity {
     public void clickNine(View view) {
         addNumberToEquation("9");
     }
+    public void clickDel(View view) {
+        del();
+    }
+    public void clickAdd(View view) {
+        addSymbolToEquation("+");
+    }
+    public void clickSubtract(View view) {
+        addSymbolToEquation("-");
+    }
+    public void clickDivide(View view) {
+        addSymbolToEquation("÷");
+    }
+    public void clickMultiply(View view) {
+        addSymbolToEquation("×");
+    }
+    public void clickPeriod(View view) {
+        addSymbolToEquation(".");
+    }
+    public void clickEquals(View view) {
+        parseEquation();
+    }
 
     public void addNumberToEquation(String number) {
-        if(equationString == "0") {
+        if(equationString.equals("0")) {
             equationString = number;
         }
         else {
@@ -55,9 +89,93 @@ public class CalculatorActivity extends AppCompatActivity {
         }
         updateOutput();
     }
-    public void updateOutput() {
-        TextView output = (TextView)findViewById(R.id.outputText);
+    public void addSymbolToEquation(String symbol) {
+        //÷  ×  +  -
+        if(symbol.equals(PLUS)) {
+            if (doesntEndWithNumber(equationString)) {
+                del();
+                equationString += PLUS;
+            } else {
+                equationString += PLUS;
+            }
+        }
+        if(symbol.equals(MINUS)) {
+            if (doesntEndWithNumber(equationString)) {
+                del();
+                equationString += MINUS;
+            } else {
+                equationString += MINUS;
+            }
+        }
+        if(symbol.equals(DIVIDE)) {
+            if (doesntEndWithNumber(equationString)) {
+                del();
+                equationString += DIVIDE;
+            } else {
+                equationString += DIVIDE;
+            }
+        }
+        if(symbol.equals(TIMES)) {
+            if (doesntEndWithNumber(equationString)) {
+                del();
+                equationString += TIMES;
+            } else  {
+                equationString += TIMES;
+            }
+        }
+        if(symbol.equals(".")) {
+            //TODO we need a more complicated check for a period, since you can only have one per number.
+            if(doesntEndWithNumber(equationString)) {
+                del();
+                equationString += ".";
+            } else {
+                equationString += ".";
+            }
+        }
+        updateOutput();
+    }
+    public void del() {
+        if(output.getText().length() > 1) {
+            equationString = output.getText().subSequence(0, output.getText().length() - 1).toString();
+        } else if(output.getText().length() > 0) {
+            equationString = "0";
+        }
         output.setText(equationString);
+    }
+    public void updateOutput() {
 
+        output.setText(equationString);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        while(output.getWidth() > width) {
+            output.setTextSize(output.getTextSize()-1);
+        }
+        if(!doesntEndWithNumber(equationString)) {
+            parseEquation();
+        }
+
+    }
+    public boolean doesntEndWithNumber(String equation) {
+        //TODO add other mathematical constants that could be a number
+        if (      !equation.endsWith("1")
+                &&!equation.endsWith("2")
+                &&!equation.endsWith("3")
+                &&!equation.endsWith("4")
+                &&!equation.endsWith("5")
+                &&!equation.endsWith("6")
+                &&!equation.endsWith("7")
+                &&!equation.endsWith("8")
+                &&!equation.endsWith("9")
+                &&!equation.endsWith("0")
+                &&!equation.endsWith(")")
+                ) {
+            return true;
+        } else {
+            return false;
+        }
+        }
+    public void parseEquation() {
+        //Parse output in new class
+        answer.setText(EquationParser.Parse(equationString));
     }
 }
---
+++ app/src/main/java/com/sub6resources/allmath/math/EquationParser.java
@@ -0,0 +1,11 @@
+package com.sub6resources.allmath.math;
+
+/**
+  Created by Matthew Whitaker on 4/13/2017. Copyright (c) 2017 Sub 6 Resources
+ */
+
+public final class EquationParser {
+    public static String Parse(String equation) {
+        return "Code Doesn't Exist";
+    }
+}
